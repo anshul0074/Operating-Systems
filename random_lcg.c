0443cbe88cdf560da242fb21f31850ccb3f82476
anshul0074 <88040808+anshul0074@users.noreply.github.com>
2022-01-04 10:30:48 +0530

    Random number generation using Linear Congruency Generation.

---
+++ random_lcg.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define int long long 
+
+signed main(){
+    int x=7,a=3,b=5,m=1000007;
+    freopen("output1.txt","w",stdout);
+    for(int i=0;i<100000;i++){
+        printf("%d ",x);
+        x=(a*x+b)%m;
+    }
+}
